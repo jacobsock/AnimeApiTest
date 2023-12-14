62964a55df9575e420971cf90f7888335862b43c
Jacob Sock <jacobmsock@gmail.com>
2023-12-14 08:55:18 -0500

    Initial Commit

---
+++ AnimeApiTest/AnimeApiTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeApiTestApp.swift
+//  AnimeApiTest
+//
+//  Created by Jacob M Sock  on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimeApiTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimeApiTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimeApiTest
+//
+//  Created by Jacob M Sock  on 12/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

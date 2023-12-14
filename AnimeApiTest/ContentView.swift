ec86e4b1422e2733ed08896811240ac09f5d321a
Jacob Sock <jacobmsock@gmail.com>
2023-12-14 09:16:43 -0500

    add logic to call random anime quote

--- AnimeApiTest/ContentView.swift
+++ AnimeApiTest/ContentView.swift
@@ -7,18 +7,66 @@
 
 import SwiftUI
 
+import SwiftUI
+
+struct AnimeData: Codable {
+    let anime: String
+    let character: String
+    let quote: String
+}
+
 struct ContentView: View {
+    @State private var animeData: AnimeData?
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Fetch Anime Quote") {
+                fetchAnimeData()
+            }
+            .padding()
+
+            if let animeData = animeData {
+                Text("Anime: \(animeData.anime)")
+                Text("Character: \(animeData.character)")
+                Text("Quote: \(animeData.quote)")
+            }
         }
-        .padding()
+    }
+
+    private func fetchAnimeData() {
+        guard let url = URL(string: "https://animechan.xyz/api/random") else {
+            print("Invalid URL")
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                return
+            }
+
+            guard let data = data else {
+                print("No data received")
+                return
+            }
+
+            do {
+                let decoder = JSONDecoder()
+                let animeData = try decoder.decode(AnimeData.self, from: data)
+                DispatchQueue.main.async {
+                    self.animeData = animeData
+                }
+            } catch {
+                print("Error decoding JSON: \(error.localizedDescription)")
+            }
+        }.resume()
     }
 }
 
+
+
 #Preview {
-    ContentView()
+        ContentView()
+    
 }
+
